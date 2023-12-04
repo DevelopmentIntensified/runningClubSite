5c7763afcdd58e682cfe8e6139655d21faeddfc1
MIRP <developmentIntensified@gmail.com>
2023-12-03 20:27:43 -0500

    adding nav, removing old parts

---
+++ src/Navbar/nav.js
@@ -0,0 +1,39 @@
+export class Navbar extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    const shadow = this.attachShadow({ mode: 'open' });
+    const headerElement = document.createElement('header');
+    const nav = document.createElement('nav');
+
+    nav.innerHTML = `
+      <a href="https://vitejs.dev" target="_blank">test</a>`;
+
+    const style = document.createElement('style');
+
+    style.textContent = `
+      nav {
+        align-items: center;
+        justify-content: space-between;
+        background-color: #941b1b;
+        color: white;
+      }
+      header {
+        display:block;
+        width: 100%;
+      }
+      a {
+        color: white;
+        text-decoration: none;
+        font-size: 1.5rem;
+        padding: 1rem;
+      }`
+    shadow.appendChild(style);
+    shadow.appendChild(headerElement);
+    headerElement.appendChild(nav);
+    console.log(style.isConnected);
+  }
+}
+
