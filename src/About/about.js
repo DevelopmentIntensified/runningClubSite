90bf1738be8fc51bc43a50771c29457c7649c795
MIRP <developmentIntensified@gmail.com>
2024-01-28 15:16:44 -0500

    adding about page

--- src/About/about.js
+++ src/About/about.js
@@ -1,16 +1,20 @@
+import aboutStyles from './about.css?raw';
+import aboutHtml from './about.html?raw';
+
 export class About extends HTMLElement {
     constructor() {
         super();
     }
 
-    connectedCallback() { 
-    const shadow = this.attachShadow({ mode: 'open' });
-    const style = document.createElement('style');
-const homePage = document.createElement('div');
-        homePage.innerHTML = "About"
+    connectedCallback() {
+        const shadow = this.attachShadow({ mode: 'open' });
+        const style = document.createElement('style');
+        const homePage = document.createElement('div');
+        homePage.innerHTML = aboutHtml
+        style.innerHTML = aboutStyles
 
-    shadow.appendChild(style);
-       shadow.appendChild(homePage) 
+        shadow.appendChild(style);
+        shadow.appendChild(homePage)
     }
 
 }
