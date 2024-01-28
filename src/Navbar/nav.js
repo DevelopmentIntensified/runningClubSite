874caf0ba3d5d9978b641776bc1aed1186f930cf
MIRP <developmentIntensified@gmail.com>
2024-01-27 21:38:07 -0500

    fixed nav link

--- src/Navbar/nav.js
+++ src/Navbar/nav.js
@@ -22,7 +22,7 @@ export class Navbar extends HTMLElement {
     const logoLink = document.createElement('a');
 
     logo.src = logoSrc
-    logoLink.href = "https:\\www.liberty.edu"
+    logoLink.href = "https:\\\\www.liberty.edu"
     logoLink.className = "logoLink"
     logoLink.setAttribute("target", "_blank")
     logoLink.setAttribute("rel", "noreferrer")
