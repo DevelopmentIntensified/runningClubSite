edde6c8b8163c25c7d10267a8dc8b8e526baf119
mpetke <mpetke@liberty.edu>
2024-02-07 17:19:05 -0500

    mobile improvements and style improvements

--- src/Navbar/nav.js
+++ src/Navbar/nav.js
@@ -20,6 +20,8 @@ export class Navbar extends HTMLElement {
     const style = document.createElement('style');
     const logo = document.createElement('img');
     const logoLink = document.createElement('a');
+    const mobileNav = document.createElement('nav');
+    const mobileSidebar = document.createElement('div');
 
     logo.src = logoSrc
     logoLink.href = "https:\\\\www.liberty.edu"
@@ -34,7 +36,7 @@ export class Navbar extends HTMLElement {
       a.textContent = route.name;
       a.href = route.path;
       a.className = "tab"
-      a.onclick = function(e) {
+      a.onclick = function(_e) {
         history.pushState(route.path,"")
       }
       li.appendChild(a);
@@ -42,10 +44,29 @@ export class Navbar extends HTMLElement {
     })
 
     logoLink.appendChild(logo)
+
+    const mobileLogoLink = logoLink.cloneNode(true) 
+    const mobileLinkList = linkList.cloneNode(true) 
+
     nav.appendChild(logoLink);
     nav.appendChild(linkList);
     nav.classList.add('not-mobile');
 
+    mobileNav.classList.add('mobile');
+    mobileNav.appendChild(mobileLogoLink);
+    
+    mobileNav.onclick = function(e) {
+      mobileSidebar.classList.toggle('active');
+    }
+    
+    mobileSidebar.classList.add('mobile-sidebar');
+    mobileSidebar.appendChild(mobileLinkList);
+    mobileSidebar.onclick = function(e) {
+      mobileSidebar.classList.toggle('active');
+    }
+
+    shadow.appendChild(mobileSidebar);
+    shadow.appendChild(mobileNav);
     shadow.appendChild(style);
     shadow.appendChild(headerElement);
     headerElement.appendChild(nav);
