b9dd847100c035f489cb229ea982193e220cdcb5
developmentIntensified <developmentIntensified@gmail.com>
2024-12-29 01:29:26 -0500

    quick fix

--- src/lib/utils/getUrl.ts
+++ src/lib/utils/getUrl.ts
@@ -3,7 +3,9 @@ import { dev } from '$app/environment';
 export function getUrl() {
   if (dev) {
     return 'http://localhost:5173/';
+  } else if (process.env.ENV === 'test') {
+    return 'https://running-club-site-git-test-developmentintensifieds-projects.vercel.app/';
   } else {
-    return '';
+    return "https://running-club-site-developmentintensifieds-projects.vercel.app/"
   }
 }
