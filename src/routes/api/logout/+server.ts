72e36a4e8d3735f9bb334513f4349597f693a2a1
developmentintensified <developmentintensified@gmail.com>
2026-04-10 12:22:17 -0400

    Delete session from DB on logout

--- src/routes/api/logout/+server.ts
+++ src/routes/api/logout/+server.ts
@@ -1,5 +1,8 @@
 import { lucia } from '$lib/server/auth';
 import { redirect } from '@sveltejs/kit';
+import { db } from '$lib/server/db';
+import { sessions } from '$lib/server/db/schema';
+import { eq } from 'drizzle-orm';
 import type { RequestHandler } from './$types';
 
 export const POST: RequestHandler = async ({ locals }) => {
@@ -7,6 +10,7 @@ export const POST: RequestHandler = async ({ locals }) => {
   if (!session) {
     return redirect(302, '/');
   }
+  await db.delete(sessions).where(eq(sessions.id, session.id));
   await lucia.invalidateSession(session.id);
   locals.session = null;
   locals.user = null;
