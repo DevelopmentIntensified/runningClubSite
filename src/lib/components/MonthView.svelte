677134f377546fba9f21e0a2ead187b10863bcc5
developmentintensified <developmentintensified@gmail.com>
2026-04-12 17:08:56 -0400

    Revert: show multi-day events on each date

--- src/lib/components/MonthView.svelte
+++ src/lib/components/MonthView.svelte
@@ -90,11 +90,7 @@
       {@const dayEvents = events.filter((event) => {
         const eventStart = event.start.startOf('day');
         const eventEnd = event.end.startOf('day');
-        if (isAdmin) {
-          return date >= eventStart && date <= eventEnd;
-        } else {
-          return date.hasSame(eventStart, 'day');
-        }
+        return date >= eventStart && date <= eventEnd;
       })}
       <div
         class="group min-h-[5.5rem] border-t border-transparent bg-white p-1.5 transition-colors hover:bg-slate-50/90 sm:min-h-[6.5rem] sm:p-2"
